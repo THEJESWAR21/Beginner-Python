1b76c74ff53350a8c76295d9a7f946b0f8fef108
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-04-28 11:32:06 +0530

    Score System

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,29 +2,56 @@ import random
 
 option = ["Rock","Paper","Scissors"]
 
-user_choice = input("Your Choice (Rock, Paper or Scissors): ")
-computer_choice = random.choice(option)
-
-print("Your Choice:",user_choice)
-print("Computer Choice:",computer_choice)
-
+# Scores
 user = 0
 computer = 0
 
-if user_choice == computer_choice:
-    print("DRAW!!!",end="")
-    result="Draw"
-elif user_choice == "Paper" and computer_choice == "Rock":
-    print("YOU WIN!!!",end="")
-    result="User_Wins"
-elif user_choice == "Scissors" and computer_choice == "Paper":
-    print("YOU WIN!!",end="")
-    result="User_Wins"
-elif user_choice == "Rock" and computer_choice == "Scissors":
-    print("YOU WIN!!",end="")
-    result="User_Wins"
+while True:
 
-else:
-    print("COMPUTER WINS!")
-    result=("Computer_Wins")
+    # Inputs
+    user_choice = input("Your Choice (Rock, Paper or Scissors): ")
+    computer_choice = random.choice(option)
 
+    # Display Choices
+    print("Your Choice:",user_choice)
+    print("Computer Choice:",computer_choice)
+
+    # Draw
+    if user_choice == computer_choice:
+        print("DRAW!!!")
+        result="Draw"
+
+    # Choices that makes the user win
+    elif user_choice == "Paper" and computer_choice == "Rock":
+        print("YOU WIN!!!")
+        result="User_Wins"
+        user = user + 1
+    elif user_choice == "Scissors" and computer_choice == "Paper":
+        print("YOU WIN!!")
+        result="User_Wins"
+        user = user + 1
+    elif user_choice == "Rock" and computer_choice == "Scissors":
+        print("YOU WIN!!",)
+        result="User_Wins"
+        user = user + 1
+
+    else:
+        print("COMPUTER WINS!")
+        result=("Computer_Wins")
+        computer = computer + 1
+        
+    if user < computer:
+        print("Your Points", user) 
+        print("Computer's Points", computer)
+        print("COMPUTER WINS!")
+    elif user == computer:
+        print("Your Points", user)
+        print("Computer's Points", computer)
+        print("DRAW!!", )
+    else:
+        print("Your Points", user,)
+        print("Computer's Points", computer)
+        print("YOU WINS!") 
+    
+    if user == 3 or computer == 3:
+        break
